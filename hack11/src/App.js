74724f2d71ac46c2e32f72fe457ccae7ed8e6a55
Devin Lust <lust.65@osu.edu>
2023-10-28 10:30:23 -0400

    Added grid

--- hack11/src/App.js
+++ hack11/src/App.js
@@ -1,8 +1,10 @@
-
+import Grid from './Grid.js'
+import './style/index.css'
 function App() {
   return (
     <div className="App bg-slate-600">
       test
+      <Grid></Grid>
     </div>
   );
 }
