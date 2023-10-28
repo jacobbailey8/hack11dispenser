bf2f5003b59a25634be557a67359dffb42e090e6
jacobbailey8 <jbaileydsi87@gmail.com>
2023-10-28 09:56:19 -0400

    changes

--- hack11/src/App.js
+++ hack11/src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="font-bold underline">test</div>
+    <div className="App bg-slate-600">
+      test
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {
       fontFamily: {
-        space: ['Space Grotesk', 'sans-serif'],
+
       },
       colors: {
-        purple: 'rgb(132, 0, 152)',
-        overlay: 'rgb(0,0,0,0.4)',
-        gradient: ' linear-gradient(217deg, rgba(29, 0, 57, 0.8) 10%, rgba(4, 1, 14, 0.5) 60.71%, rgba(33, 6, 125, 0.5)), linear-gradient(127deg, rgba(39, 1, 114, 0.8) 20%, rgba(49, 0, 97, 0.5) 70.71%, rgba(115, 6, 125, 0.5)), linear-gradient(336deg, rgba(11, 1, 59, 0.8), rgba(8, 8, 173, 0.5) 70.71%, rgba(6, 34, 125, 0.5));'
+
       },
     },
   },
